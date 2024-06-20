9bfec1f3a7ccd755327f6719b4fa0b6a1b9afb10
Driss <drissgazzaoui1986@hotmail.com>
2024-06-20 14:07:16 +0200

    tp09 AffichageDonnees fini

---
+++ tp09/tp09-script.js
@@ -0,0 +1,32 @@
+function fetchData() {
+    const req = new XMLHttpRequest()
+    req.open('GET', 'https://restcountries.com/v2/all')
+
+    req.onreadystatechange = function () {
+        if (req.readyState === 4 && req.status === 200) {
+            let countries = JSON.parse(req.responseText)
+            displayData(countries)
+        }
+    }
+    req.send();
+}
+
+function displayData(countries) {
+    const container = document.getElementById('data-container');
+    let htmlContent = '';
+
+    countries.forEach(country => {
+        htmlContent += `<div class="country">
+            <img src="${country.flags.svg}" alt="Drapeau de ${country.name}">
+            <p><strong>Nom:</strong> ${country.name}</p>
+            <p><strong>Capitale:</strong> ${country.capital}</p>
+            <p><strong>Région:</strong> ${country.region}</p>
+
+            <p><strong>Drapeau:</strong> <img src="${country.flags.png}" alt="Drapeau de ${country.name}" width="32"></p>
+            <hr>
+        </div>`;
+    });
+
+    container.innerHTML = htmlContent
+
+}
