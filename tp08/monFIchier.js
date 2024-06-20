4dabe45696e2afd72fac5c9b83fb49ac2b42f763
Driss <drissgazzaoui1986@hotmail.com>
2024-06-20 13:14:43 +0200

    tp08 fini

---
+++ tp08/monFIchier.js
@@ -0,0 +1,33 @@
+function initialiserDonnees() {
+    let data = {
+        "polluant": "CO2",
+        "unite": "milliards de tonnes",
+        "annee": 2017,
+        "pays": [
+            { "nom": "Chine", "valeur": 9.26, "pourcentage": 28.2, "code": "cn" },
+            { "nom": "Etats-Unis", "valeur": 4.76, "pourcentage": 14.5, "code": "us" },
+            { "nom": "Inde", "valeur": 2.16, "pourcentage": 6.6, "code": "in" },
+            { "nom": "Russie", "valeur": 1.54, "pourcentage": 4.7, "code": "ru" },
+            { "nom": "Japon", "valeur": 1.13, "pourcentage": 3.4, "code": "jp" },
+            { "nom": "Allemagne", "valeur": 0.72, "pourcentage": 2.2, "code": "de" },
+            { "nom": "Corée du Sud", "valeur": 0.6, "pourcentage": 1.8, "code": "kr" },
+            { "nom": "Iran", "valeur": 0.57, "pourcentage": 1.7, "code": "ir" },
+            { "nom": "Canada", "valeur": 0.55, "pourcentage": 1.7, "code": "ca" }
+        ]
+    }
+
+    document.getElementById("titre").textContent = `${data.polluant} (${data.unite})`;
+    document.getElementById('annee').textContent = `${data.annee}`;
+
+    let listePaysElement = document.getElementById('listePays');
+    let balise = `<ul>`
+
+    data.pays.forEach(p =>{
+        balise += `<li>  
+        <strong>${p.nom}</strong> (code: ${p.code})<br>
+        Valeur: ${p.valeur} ${data.unite}, Pourcentage: ${p.pourcentage}%
+        </li>`;        
+    })
+    balise += `</ul>`
+    listePaysElement.innerHTML =balise
+}
